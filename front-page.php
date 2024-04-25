5438baf91e928ecee7b27d0438cdc4aa1f167bf6
neisha rose <neisha1618@gmail.com>
2024-04-24 22:04:28 -0500

    secondary hero and style has been added

--- front-page.php
+++ front-page.php
@@ -1,7 +1,4 @@
-<link
-  rel="stylesheet"
-  href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"
-/>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
 
 <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
 
@@ -20,11 +17,39 @@ $imageID = get_field('hero_image');
 $size = 'full';
 
 if ($imageID) {
-    echo wp_get_attachment_image($imageID, $size);
+  echo wp_get_attachment_image($imageID, $size);
 }
 ?>
 
 
+
+
+<div class="image-container secondary-hero">
+ <?php if (have_rows('secondary_hero')): ?>
+    <?php $i = 1; ?>
+    <?php while (have_rows('secondary_hero')):
+      the_row();
+
+
+      ?>
+      <!-- Background image -->
+
+      <div class="image-wrapper image-<?php echo $i; ?>">
+
+        <?php
+        $imageID = get_sub_field('hero_gallery');
+        $size = 'full';
+        echo wp_get_attachment_image($imageID, $size, false, array('class' => 'background-image'));
+        ?>
+
+        <div class="overlay-image">
+        </div>
+
+      </div>
+      <?php $i++; ?>
+    <?php endwhile; ?>
+  <?php endif; ?>
+</div>
 
 
 
@@ -39,62 +64,44 @@ if ($imageID) {
 
 <div id="content" class="content">
 
-    <div class="inner-content">
+  <div class="inner-content">
 
-        <main id="main" class="main" role="main">
-           <div class="hero">
-                <div class="archive-wrapper">
-					<div class="grid-container">
-						<div class="grid-x align-center">
-							<div class="medium-10 grid-x grid-padding-x align-middle">
-								<div class="medium-6 cell">
-                                <header>
-                                   
-                                   
-                                </header>
-							    </div>
-                            </div>	
-						</div>
-					</div>
-				</div>
-           </div>
-            
-
-          
-            <?php get_template_part('parts/content-blocks/dial'); ?>
-            <?php get_template_part('parts/content-blocks/content_grid'); ?>
-
+    <main id="main" class="main" role="main">
       
+      <?php get_template_part('parts/content-blocks/dial'); ?>
+      <?php get_template_part('parts/content-blocks/content_grid'); ?>
 
-            <?php get_template_part('parts/content-blocks/content_slider'); ?>
-<!-- Slider main container -->
 
-  <!-- If we need pagination -->
-  <!-- <div class="swiper-pagination"></div> -->
 
-  <!-- If we need navigation buttons
+      <?php get_template_part('parts/content-blocks/content_slider'); ?>
+      <!-- Slider main container -->
+
+      <!-- If we need pagination -->
+      <!-- <div class="swiper-pagination"></div> -->
+
+      <!-- If we need navigation buttons
   <div class="swiper-button-prev"></div>
   <div class="swiper-button-next"></div> -->
 
-  <!-- If we need scrollbar -->
-  <!-- <div class="swiper-scrollbar"></div>
+      <!-- If we need scrollbar -->
+      <!-- <div class="swiper-scrollbar"></div>
 </div> -->
 
 
 
-        </main> <!-- end #main -->
+    </main> <!-- end #main -->
 
-        <?php get_template_part('parts/content-blocks/light-reading-block'); ?>
+    <?php get_template_part('parts/content-blocks/light-reading-block'); ?>
 
 
-    </div> <!-- end #inner-content -->
-    <?php $footerText = get_field('footer_text') ?>
+  </div> <!-- end #inner-content -->
+  <?php $footerText = get_field('footer_text') ?>
 
-    <?php get_footer(null, $footerText); ?>
+  <?php get_footer(null, $footerText); ?>
 
-    
-    </div> <!-- end #content -->
-    <?php wp_link_pages(); ?>
-    <div id="logo-footer">
-        <?php get_template_part('parts/content-blocks/logo-footer'); ?>
+
+</div> <!-- end #content -->
+<?php wp_link_pages(); ?>
+<div id="logo-footer">
+  <?php get_template_part('parts/content-blocks/logo-footer'); ?>
 </div>

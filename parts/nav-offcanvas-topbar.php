b210b87cf3f38b5c45909d03cc84ebc7cac66c0e
neisha rose <neisha1618@gmail.com>
2024-04-28 01:10:32 -0500

    (svg) logos added to navbar and hero,  assests were added to image/src

--- parts/nav-offcanvas-topbar.php
+++ parts/nav-offcanvas-topbar.php
@@ -7,17 +7,20 @@
 ?>
 
 <div class="top-bar" id="top-bar-menu">
-	<div class="top-bar-left float-left">
-		<ul class="menu">
-			<li><a class="menu-text" href="<?php echo home_url(); ?>"><?php bloginfo('name'); ?></a></li>
-		</ul>
-	</div>
-	<div class="top-bar-right show-for-medium">
-		<?php joints_top_nav(); ?>	
-	</div>
-	<div class="top-bar-right float-right show-for-small-only">
-		<ul class="menu">
-			<li><button class="menu-icon" type="button" data-toggle="off-canvas"></button></li>
-		</ul>
-	</div>
+  <div class="top-bar-left float-left">
+    <img class="nav-logo" src="<?php echo get_template_directory_uri(); ?>/assets/images/src/nav-logo.svg"  />
+    <ul class="menu">
+    </ul>
+  </div>
+  <div class="top-bar-right show-for-medium">
+    <?php joints_top_nav(); ?>
+  </div>
+  <div class="top-bar-right float-right show-for-small-only">
+    <ul class="menu">
+      <li><button class="menu-icon" type="button" data-toggle="off-canvas"></button></li>
+    </ul>
+   
+  </div>
+  <iframe class="nav-social" src="<?php echo get_template_directory_uri(); ?>/assets/images/src/social-media.svg" frameBorder="0"></iframe>
 </div>
+
